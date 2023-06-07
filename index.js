e31588de3cc74670733c81df2193943d2e55eb1a
Michael Salas <michaelsalasalcedo15@gmail.com>
2023-06-06 23:22:41 -0500

    deploy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
@@ -27,6 +28,10 @@ app.use( '/api/medicos', require('./routes/medicos'));
 app.use( '/api/todo', require('./routes/busquedas'));
 app.use( '/api/upload', require('./routes/uploads'));
 app.use( '/api/login', require('./routes/auth'));
+
+app.get('*', (req,res)=>{
+    res.sendFile( path.resolve(__dirname,'public/index.html') );
+});
 
 
 app.listen( process.env.PORT, ()=>{
